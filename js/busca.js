b244967879ca63c2df456d04e7fe487468a791de
Dharion Soares <dharionsoares@hotmail.com>
2018-01-27 14:08:58 -0200

    Update busca.js

--- js/busca.js
+++ js/busca.js
@@ -3,7 +3,7 @@ var busca = $(".busca");
 // pega o valor inicial da altura da barra de busca que está no estilo da classe
 var alturaBuscaDesativada = busca.css("height");
 
-var tituloBusca = $("#titulo-busca");
+var tituloBusca = $(".titulo-busca");
 var formBusca = $(".form-busca");
 
 var botaoBuscaAtivado, botaoBuscaClicar;
@@ -17,7 +17,7 @@ $(function(){
 });
 
 $(".botao-mostra-busca").click(function(){
-  busca.css("height", "");
+  busca.css("height", ""); // limpa o tamanho para ficar de acordo com a ativacao/desativacao
 
   if (botaoBuscaAtivado) {
       botaoBuscaClicar = true // ativa o clique para mostrar a busca
@@ -26,41 +26,25 @@ $(".botao-mostra-busca").click(function(){
   }
 
   if (botaoBuscaClicar) {
-    ativaBusca();
+    alteraStatusBusca("on", "off");
   } else {
-    desativaBusca();
+    alteraStatusBusca("off", "on");
   }
 });
 
-function ativaBusca() {
-  busca.addClass("busca-ativada");
-  busca.removeClass("busca-desativada");
+function alteraStatusBusca(mostra, esconde) {
+  if (mostra == "on") {
+    busca.addClass("busca-ativada").removeClass("busca-desativada");
+  } else {
+    busca.addClass("busca-desativada").removeClass("busca-ativada").css("height", alturaBuscaDesativada);
+  }
 
-  tituloBusca.css("visibility", "visible");
-  tituloBusca.addClass("ativado-slow");
-  tituloBusca.removeClass("desativado-slow");
+  tituloBusca.addClass(mostra).removeClass(esconde);
+  formBusca.addClass(mostra).removeClass(esconde);
 
-  formBusca.css("visibility", "visible");
-  formBusca.addClass("ativado-slow");
-  formBusca.removeClass("desativado-slow");
-
-  // desativa o botao para no proximo clique desativar a busca
-  botaoBuscaAtivado = false;
-}
-
-function desativaBusca() {
-  busca.addClass("busca-desativada");
-  busca.css("height", alturaBuscaDesativada);
-  busca.removeClass("busca-ativada");
-
-  tituloBusca.css("visibility", "hidden");
-  tituloBusca.addClass("desativado-slow");
-  tituloBusca.removeClass("ativado-slow");
-
-  formBusca.css("visibility", "hidden");
-  formBusca.addClass("desativado-slow");
-  formBusca.removeClass("ativado-slow");
-
-  // ativa o botao para no proximo clique ativar a busca
-  botaoBuscaAtivado = true;
+  if (mostra == "on") {
+    botaoBuscaAtivado = false; // desativa o botao para no proximo clique desativar a busca
+  } else {
+    botaoBuscaAtivado = true; // ativa o botao para no proximo clique ativar a busca
+  }
 }
